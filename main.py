da42b69ea6aa52f804c702b7b72d077893c3cdbd
Maciej Handke <maciejhandke11@gmail.com>
2022-04-18 12:59:54 +0200

    Connect GUI with DICOM module

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+import skimage.io
 import streamlit as st
 import numpy as np
 from skimage.io import imread
 import tomograph_module as tomograph
+from dicom_module import DICOM
+
 
 st.set_page_config(page_title="IWM - Tomograf komputerowy")
 
@@ -20,7 +23,17 @@ def normalization(data):
 
 
 if input_image_upload:
-    image = imread(input_image_upload, True)
+    dicom = DICOM()
+    if input_image_upload.type == "application/octet-stream":
+        with st.expander("Informacje o pacjencie"):
+            image, info = dicom.dicom_open(input_image_upload)
+            st.write(f"Imię i nazwisko: {str(info.get('PatientName'))}")
+            st.write(f"ID: {str(info.get('PatientID'))}")
+            st.write(f"Data badania: {str(info.get('PatientBirthDate'))}")
+            st.write(f"Komentarz: {str(info.get('AdditionalPatientHistory'))}")
+            image = normalization(image)
+    else:
+        image = imread(input_image_upload, True)
     st.image(image, "Oryginalny obraz")
     ct_scanner = tomograph.CTScanner(image, scans_number, detectors_number, spread_size)
     output_image, sinogram = ct_scanner.main_process()
@@ -40,3 +53,26 @@ if input_image_upload:
         st.image(normalization(output_image[output_image_iteration - 1]), "Obraz końcowy")
     else:
         st.image(normalization(output_image[len(output_image) - 1]), "Obraz końcowy")
+
+    with st.expander("Zapisz jako plik DICOM"):
+        patient_name = st.text_input("Imię i nazwisko")
+        patient_id = st.text_input("ID")
+        patient_birthdate = st.date_input("Data badania")
+        comment = st.text_area("Komentarz")
+        patient_info = {
+            "PatientName": patient_name,
+            "PatientID": patient_id,
+            "PatientBirthDate": str(patient_birthdate),
+            "AdditionalPatientHistory": comment
+        }
+        if len(patient_name) and len(patient_id):
+            file_name = f'{patient_name} {patient_id}.dcm'
+        else:
+            file_name = "unnamed.dcm"
+        save_image = st.radio("Zapisz jako DICOM ", ["Obraz wejściowy", "Obraz wyjściowy"])
+        if save_image == "Obraz wyjściowy":
+            if st.button("Zapisz"):
+                dicom.dicom_save(file_name, np.array(normalization(output_image[len(output_image) - 1])), patient_info)
+        else:
+            if st.button("Zapisz"):
+                dicom.dicom_save(file_name, image, patient_info)
